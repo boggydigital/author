99796b592618caef2f1d127201c588ab9824e995
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-09-26 21:14:38 -0700

    Adding successPath to bouncer

--- bouncer.go
+++ bouncer.go
@@ -2,16 +2,18 @@ package author
 
 import (
 	"net/http"
+	"time"
 )
 
 const CookieKeySession = "Session"
 
 type Bouncer struct {
-	author    *Authenticator
-	loginPath string
+	author      *Authenticator
+	loginPath   string
+	successPath string
 }
 
-func NewBouncer(dir string, rolePermissions map[string][]Permission, loginPath string) (*Bouncer, error) {
+func NewBouncer(dir string, rolePermissions map[string][]Permission, loginPath, successPath string) (*Bouncer, error) {
 
 	author, err := NewAuthenticator(dir, rolePermissions)
 	if err != nil {
@@ -19,8 +21,9 @@ func NewBouncer(dir string, rolePermissions map[string][]Permission, loginPath s
 	}
 
 	return &Bouncer{
-		author:    author,
-		loginPath: loginPath,
+		author:      author,
+		loginPath:   loginPath,
+		successPath: successPath,
 	}, nil
 }
 
@@ -39,7 +42,7 @@ func ValidateSession(b *Bouncer, next http.Handler) http.Handler {
 					continue
 				}
 
-				if err := b.author.ValidateSession(cookie.Value); err != nil {
+				if err = b.author.ValidateSession(cookie.Value); err != nil {
 					http.Error(w, err.Error(), http.StatusBadRequest)
 					return
 				}
@@ -51,4 +54,43 @@ func ValidateSession(b *Bouncer, next http.Handler) http.Handler {
 
 		http.Redirect(w, r, b.loginPath, http.StatusTemporaryRedirect)
 	})
+}
+
+func (b *Bouncer) Authenticate(w http.ResponseWriter, r *http.Request) {
+
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if username := r.FormValue(UsernameParam); username != "" {
+		if password := r.FormValue(PasswordParam); password != "" {
+
+			if session, err := b.author.CreateSession(username, password); err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			} else {
+
+				expires := time.Now().UTC().Add(defaultSessionDurationDays * time.Hour * 24)
+
+				cookie := &http.Cookie{
+					Name:        CookieKeySession,
+					Value:       session,
+					Expires:     expires,
+					Secure:      true,
+					HttpOnly:    true,
+					SameSite:    http.SameSiteStrictMode,
+					Partitioned: false,
+				}
+
+				http.SetCookie(w, cookie)
+				//http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+
+				return
+			}
+
+		}
+	}
+
+	http.Error(w, "Unauthorized access attempt detected", http.StatusUnauthorized)
 }
